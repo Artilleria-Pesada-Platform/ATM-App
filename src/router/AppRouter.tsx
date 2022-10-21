4c634591d6970827022ad91439e023924c7b628f
Eduardodac <eduardo.dac@outlook.com>
2022-10-21 01:10:53 -0500

    Crea LandingPage

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom"
+import { LandingPage } from "../pages/LandingPage"
 
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Pagina />} />
+                <Route path="/" element={<LandingPage />} />
                 <Route path="/" element={<Navigate to={"/"} />} />
             </Routes>
         </BrowserRouter>
