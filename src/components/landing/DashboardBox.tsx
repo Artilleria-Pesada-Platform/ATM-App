aa029c90ad6bcb1c5e908f242859cca187b66c7e
Eduardodac <eduardo.dac@outlook.com>
2022-10-21 02:02:45 -0500

    Crea el componente dashboardBox

---
+++ src/components/landing/DashboardBox.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DashboardBox = () => {
+  return (
+    <div className='bg-slate-900 h-10 w-97.5 m-auto'>DashboardBox</div>
+  )
+}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { DashboardBox } from '../components/landing/DashboardBox'
 
 export const LandingPage = () => {
   return (
-    <div className=''>
-        LandingPage
+    <div className='h-full w-full'>
+        <div className='h-10 bg-slate-200 w-full flex'>
+            <span className='bg-slate-500 m-auto'>Artillería Pesada ATM</span>
+        </div>
+
+        <div className='bg-slate-500 h-5/6 w-120 sm:w-150 md:w-175 m-auto flex flex-col justify-around '>
+            <DashboardBox></DashboardBox>
+            <DashboardBox></DashboardBox>
+            <DashboardBox></DashboardBox>
+            <DashboardBox></DashboardBox>
+        </div>
     </div>
   )
 }
