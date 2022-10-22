f1cbcd9968191de38d7c8c3babaaec5fc8c02c1b
Luis Barrera <luisbarreramx@ieee.org>
2022-10-22 18:11:11 -0500

    Agrega fuentes y colores

--- src/components/shared/Log.tsx
+++ src/components/shared/Log.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 export const Log = () => {
   return (
-    <div className='bg-slate-500'>
-        <div className='flex flex-row'>
+    <div className='bg-white text-blueDark'>
+        <div className='flex flex-row font-normal '>
             <span>Nombre de Usuario</span>
             <FontAwesomeIcon className='my-8 mr-8 absolute right-0' icon={faRightToBracket}/>
         </div>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -9,16 +9,16 @@ import { Log } from '../components/shared/Log';
 export const LandingPage = () => {
   return (
     <>
-      <div className='bg-white h-screen w-full flex flex-col justify-between bold'>
+      <div className='bg-white h-screen w-full flex flex-col justify-between'>
         <div className='h-20 bg-blueDark text-white w-full flex mb-14'>
           <div className='w-20 my-1'>
             <img src={LogoAP} alt = "ArtilleriaPesadaLogo"></img>
           </div>
-          <span className='m-auto text-xl uppercase font-roboto'>Trinchera de cajeros</span>
+          <span className='m-auto text-xl uppercase  font-normal hover:font-bold'>Trinchera de cajeros APP</span>
           <Log></Log>
         </div>
 
-        <div className=' h-4/6 w-120 sm:w-150 md:w-175 m-auto flex flex-col justify-around font-roboto'>
+        <div className=' h-4/6 w-120 sm:w-150 md:w-175 m-auto flex flex-col justify-around'>
           <div className='h-1/2 flex flex-col sm:flex-row justify-around items-center '>
             <DashboardBox titulo='Analista' logo={logoPl} link = "/Analisis"></DashboardBox>
             <DashboardBox titulo='Gerente de Mantenimiento' logo={logoPl} link = "/Gerencia"></DashboardBox>
@@ -28,8 +28,8 @@ export const LandingPage = () => {
             <DashboardBox titulo='Mapas' logo={logoPl} link = "/Mapas"></DashboardBox>
           </div>
         </div> 
-        <footer className="static bg-blueDark h-10 rounded-t-full font-roboto flex">
-          <span className='text-white m-auto font-roboto'>Created with ♥ by Artillería Pesada</span>
+        <footer className="static bg-blueDark h-10 rounded-t-full flex">
+          <span className='text-white m-auto font-hairline'>Created with &#128151; by Artillería Pesada</span>
         </footer>
       </div>
       
