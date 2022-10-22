09a9866f31ade9564583b5e71ee2711078274f2f
Eduardodac <eduardo.dac@outlook.com>
2022-10-21 20:40:59 -0500

    Implementa react router

--- src/components/landing/DashboardBox.tsx
+++ src/components/landing/DashboardBox.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 type propType = {
   titulo:string,
+  logo: string,
+  link: string
 }
 
-export const DashboardBox = ({titulo}:propType) => {
+export const DashboardBox = ({titulo, logo, link}:propType) => {
   return (
-    <div className='bg-card text-white h-10 w-60 rounded-md sm:hover:mb-5 ease-in duration-300 ease-out p-6'>
+    <NavLink to={link} className="no-underline">
+      <div className='bg-card text-white h-10 w-60 rounded-md sm:hover:mb-5 ease-in duration-300 ease-out p-6'>
       <span>
         DashboardBox
       </span>
       {titulo}
+      <div className=''>
+        <img className = "h-20"src = {logo}></img>
+      </div>
+      
     </div>
+    </NavLink>
+    
   )
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { DashboardBox } from '../components/landing/DashboardBox'
 import LogoAP from "./../assets/LogoAP.svg"
-import placa from "./../assets/placa.svg"
+import logoPl from "./../assets/logo-pl.png";
+import { NavLink } from 'react-router-dom';
 
 export const LandingPage = () => {
   return (
@@ -11,21 +12,21 @@ export const LandingPage = () => {
           <div className='w-20 my-1'>
             <img src={LogoAP}></img>
           </div>
-          <span className='m-auto text-xl uppercase font-monstserrat'>Artillería Pesada ATM</span>
+          <span className='m-auto text-xl uppercase font-roboto'>Artillería Pesada ATM</span>
         </div>
 
         <div className=' h-4/6 w-120 sm:w-150 md:w-175 m-auto flex flex-col justify-around font-sansef'>
           <div className='h-1/2 flex flex-col sm:flex-row justify-around items-center '>
-            <DashboardBox titulo='1'></DashboardBox>
-            <DashboardBox titulo='2'></DashboardBox>
+            <DashboardBox titulo='1' logo={logoPl} link = "/dashboard1"></DashboardBox>
+            <DashboardBox titulo='2' logo={logoPl} link = "/dashboard2"></DashboardBox>
           </div>
           <div className='h-1/2 flex flex-col sm:flex-row justify-around items-center'>
-            <DashboardBox titulo='3'></DashboardBox>
-            <DashboardBox titulo='4'></DashboardBox>
+            <DashboardBox titulo='3' logo={logoPl} link = "/dashboard3"></DashboardBox>
+            <DashboardBox titulo='4' logo={logoPl} link = "/dashboard4"></DashboardBox>
           </div>
         </div> 
-        <footer className="static bg-blueDark h-14 rounded-t-full flex">
-          <span className='text-white m-auto text-4xl uppercase font-monstserrat'>hackatón bbva-artillería pesada</span>
+        <footer className="static bg-blueDark h-10 rounded-t-full font-roboto flex">
+          <span className='text-white m-auto font-roboto'>Created with ♥ by Artillería Pesada</span>
         </footer>
       </div>
       
