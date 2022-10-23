327fbab86388119a39b3d3eb9304de2b9bda93a7
Eduardodac <eduardo.dac@outlook.com>
2022-10-23 18:33:06 -0500

    Finaliza proyecto ATM-App

--- src/pages/Mapas.tsx
+++ src/pages/Mapas.tsx
@@ -1,16 +1,13 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useSetRecoilState } from 'recoil';
 import { iconStatus } from '../atoms/menuBurger';
-import { DashboardTop } from '../components/shared/DashboardTop';
-import {GoogleMap, useLoadScript, Marker} from "@react-google-maps/api"
+import logoPl from "./../assets/logo-pl.png";
+import 'primeicons/primeicons.css';
+import { DashboardBox } from '../mapas/DashboardBox';
 
 export const Mapas = () => {
 
   const setIconsStatus = useSetRecoilState(iconStatus);
-
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyCOvMQg6_0WDVnvGYP4jqgR0domTcKIvOk"
-  });
 
   useEffect(() => {
     setIconsStatus((anterior) => ({
@@ -22,17 +19,18 @@ export const Mapas = () => {
     }
     ))
   }, [])
-  return  isLoaded? (
-    <div className='flex flex-col h-10/12 text-tb'>
-
-      <div className='flex flex-row h-11/12 w-6/6 m-3 space-x-3'>
-        {/* MAPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA */}
-        <div className='rounded-lg bg-auxiliar w-full'> 
-        <GoogleMap zoom = {10} center = {{lat:44, lng:-80}} mapContainerClassName = "h-full w-full"></GoogleMap>
+  return (
+    <div className='bg-white h-screen w-full flex flex-col justify-between'>
+      <div className=' h-4/6 w-120 sm:w-150 md:w-200 m-auto flex flex-col justify-around'>
+        <div className='h-1/2 flex flex-col sm:flex-row justify-around items-center '>
+          <a href="http://34.174.250.232/mapas/mapas/marcas.html" ><DashboardBox titulo='Marcas' logo={logoPl} link="/Gerencia"></DashboardBox></a>
+          <a href="http://34.174.250.232/mapas/mapas/mapaFallas.html"><DashboardBox titulo='Mapa de Fallas' logo={logoPl} link="/BBVA"></DashboardBox></a>
+        </div>
+        <div className='h-1/2 flex flex-col sm:flex-row justify-around items-center'>
+          <a href="http://34.174.250.232/mapas/mapas/proximosMantenimiento.html"><DashboardBox titulo='Proximo Mantenimiento' logo={logoPl} link="/Cliente"></DashboardBox></a>
+          <a href="http://34.174.250.232/mapas/mapas/empresariales.html"><DashboardBox titulo='Mapas de Clientes Empresariales' logo={logoPl} link="/Mapas"></DashboardBox></a>
         </div>
       </div>
-
     </div>
-  ):
-  (<div>Is Loading...</div>)
+  )
 }
